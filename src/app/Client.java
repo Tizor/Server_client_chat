28c20c9e8cc332bf7e19f7c53044fcf9a3686ef9
Petr <peter.92@yandex.ru>
2020-11-08 10:33:49 +0300

    refactoring

--- src/app/Client.java
+++ src/app/Client.java
@@ -7,32 +7,36 @@ public class Client {
     static final int PORT = 5000;
     static final String HOST = "localhost";
 
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket(HOST, PORT);
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket(HOST, PORT);
 //отослать что-то с помощью in, и принять с помощью out, и наоборот
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        // поток чтения из сокета
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        // поток записи в сокет
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            // поток чтения из сокета
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // поток записи в сокет
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-        System.out.println("Вы что-то хотели сказать? Введите это здесь:");
+            System.out.println("Вы что-то хотели сказать? Введите это здесь:");
 
-        String word;
-        while (true){
-            word = reader.readLine();
-            out.write(word + "\n");
-            out.flush();
-            String serverWord = in.readLine();
+            String word;
+            while (true) {
+                word = reader.readLine();
+                out.write(word + "\n");
+                out.flush();
+                String serverWord = in.readLine();
 
-            System.out.println(serverWord);
-            if(word.equals("exit")) { break;}
+                System.out.println(serverWord);
+                if (word.equals("exit")) {
+                    break;
+                }
+            }
+            socket.close();
+            in.close();
+            out.close();
+            System.out.println("Клиент был закрыт...");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        socket.close();
-        in.close();
-        out.close();
-        System.out.println("Клиент был закрыт...");
-
     }
 }
--- src/app/Server.java
+++ src/app/Server.java
@@ -8,34 +8,36 @@ public class Server {
     static final int PORT = 5000;
 
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        Socket socket = serverSocket.accept();
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            Socket socket = serverSocket.accept();
 //отослать что-то с помощью in, и принять с помощью out, и наоборот
-        // поток чтения из сокета
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        // поток записи в сокет
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            // поток чтения из сокета
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // поток записи в сокет
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-        String word;
-        while (true) {
-            word = in.readLine();
-            if(word.equals("exit")) {
-                out.write("Привет, это Сервер! Подтверждаю прерывание процесса: " + word + "\n");
-                System.out.println("Клиент прервал процесс командой: " + word);
+            String word;
+            while (true) {
+                word = in.readLine();
+                if (word.equals("exit")) {
+                    out.write("Привет, это Сервер! Подтверждаю прерывание процесса: " + word + "\n");
+                    System.out.println("Клиент прервал процесс командой: " + word);
+                    out.flush();
+                    break;
+                }
+                out.write("Привет, это Сервер! Подтверждаю, вы написали : " + word + "\n");
                 out.flush();
-                break;}
-            out.write("Привет, это Сервер! Подтверждаю, вы написали : " + word + "\n");
-            out.flush();
-        System.out.println("Клиент написал: " + word);
-
+                System.out.println("Клиент написал: " + word);
+            }
+            socket.close();
+            in.close();
+            out.close();
+            serverSocket.close();
+            System.out.println("Сервер закрыт!");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        socket.close();
-        in.close();
-        out.close();
-        serverSocket.close();
-        System.out.println("Сервер закрыт!");
-
     }
 }
